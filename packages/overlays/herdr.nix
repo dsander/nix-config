e756f2c71110b47992106b55eccb2d2b7197f8de
Dominik Sander <git@dsander.de>
2026-09-17 01:50:00 +0200

    Update herdr to the latest fork branch

--- packages/overlays/herdr.nix
+++ packages/overlays/herdr.nix
@@ -1,18 +1,18 @@
-# herdr from our fork : https://github.com/dsander/herdr/tree/prefix-key-keybind
+# herdr from our fork : https://github.com/dsander/herdr/tree/add-last-tab-command
 final: prev: {
   herdr = prev.herdr.overrideAttrs (finalAttrs: prevAttrs: {
-    version = "0.8.2";
+    version = "0.9.0";
 
     src = final.fetchFromGitHub {
       owner = "dsander";
       repo = "herdr";
-      rev = "70f7a870660864b7a87b085552fafd3bd203d9a9";
-      hash = "sha256-rP84LXBuPZILJKdBH+TNwNil46/MQ0JVqfAacOPxlYU=";
+      rev = "bbafe648f8c563cdd73de1e88db8b32897103b6a";
+      hash = "sha256-HGliiOwCAxy1720OVoop6sRNjF9ijRsoeEJCF71Atpw=";
     };
 
     cargoDeps = final.rustPlatform.fetchCargoVendor {
       inherit (finalAttrs) pname version src;
-      hash = "sha256-4VThqPwYYEsGvaOKjBeL6XAC5bnNWB6oUMWP/uXc/UQ=";
+      hash = "sha256-CW/SF/cAPDv47gS5B7XbVZEE6LC9F1a2I1TLTJ4AWdw=";
     };
   });
 }
